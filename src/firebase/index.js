4e63c7b1dccc531ce8c01a1c31b337626183c35d
ayumitanaka <tanakayumi13@gmail.com>
2021-04-06 21:10:33 +0000

    fixed deploy error

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,4 +3,4 @@ import "firebase/firestore";
 import firebaseConfig from "./config";
 
 firebase.initializeApp(firebaseConfig);
-export const db = firebase.firestore;
+export const db = firebase.firestore();
